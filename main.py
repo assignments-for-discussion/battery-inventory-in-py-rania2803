722900776eaad080e65d3edc6d2c71e4487688d9
Rania <91493250+rania2803@users.noreply.github.com>
2023-12-03 17:30:16 +0530

    batteryHealthUpdate.py

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
+def calculate_state_of_health(present_capacity, rated_capacity):
+    return (present_capacity / rated_capacity) * 100
 
-def count_batteries_by_health(present_capacities):
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
+def classify_batteries(capacities):
+    health_counts = {
+        "healthy": 0,
+        "exchange": 0,
+        "failed": 0
+    }
+    
+    rated_capacity = 120  # Assume all batteries have a rated capacity of 120 Ah
 
+    for present_capacity in capacities:
+        soh = calculate_state_of_health(present_capacity, rated_capacity)
 
-def test_bucketing_by_health():
-  print("Counting batteries by SoH...\n")
-  present_capacities = [113, 116, 80, 95, 92, 70]
-  counts = count_batteries_by_health(present_capacities)
-  assert(counts["healthy"] == 2)
-  assert(counts["exchange"] == 3)
-  assert(counts["failed"] == 1)
-  print("Done counting :)")
+        if soh > 80:
+            health_counts["healthy"] += 1
+        elif 62 < soh <= 80:
+            health_counts["exchange"] += 1
+        else:
+            health_counts["failed"] += 1
 
+    return health_counts
+
+def test_battery_classification():
+    print("Classifying batteries by state of health...\n")
+    present_capacities = [113, 116, 80, 95, 92, 70]
+    counts = classify_batteries(present_capacities)
+    assert counts["healthy"] == 2
+    assert counts["exchange"] == 3
+    assert counts["failed"] == 1
+    print("Battery classification done :)")
 
 if __name__ == '__main__':
-  test_bucketing_by_health()
+    test_battery_classification()
